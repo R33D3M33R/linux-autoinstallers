c216d688d4c7ac0326db92cc3eda50e8ae6e9606
R33D3M33R <R33D3M33R@users.noreply.github.com>
2015-05-10 14:47:27 +0200

    Add script for Debian Jessie autoinstall and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # linux-autoinstallers
+ Bash scripts for quick customized GNU/Linux installation
+# htpc
+A script to convert minimal Debian Jessie install to Fileserver/Mediacenter
---
+++ htpc.sh
@@ -0,0 +1,97 @@
+#!/bin/bash 
+#Author: Andrej Mernik, 2015, http://andrej.mernik.eu/
+#License: GPLv3
+
+# errors: bash autoinstall.sh >out.txt 2>&1
+sources_list='/etc/apt/sources.list'
+tvheadend_list='/etc/apt/sources.list.d/tvheadend.list'
+grub_default='/etc/default/grub'
+nfs_exports='/etc/exports'
+homedir='/home/skrbnik/'
+xbmc_autostart="$homedir.kde/Autostart/xbmc"
+
+# the script must be run by root
+if [[ $(id -u) -ne 0 ]]; then
+    echo 'This script must be run as root!'
+    exit
+fi
+
+# enable the non-free and contrib repositories
+if grep -q 'main contrib non-free' $sources_list; then
+    echo 'Non-free and contrib repositories already enabled!'
+else
+  echo 'Enabling non-free and contrib repositories ...'
+  sed -i.bak 's/main/main contrib non-free/' $sources_list
+fi
+
+# add the tvheadend repository
+if [[ -f $tvheadend_list ]]; then
+    echo 'Tvheadend repository already enabled!'
+else
+  echo 'Enabling tvheadend repository ...'
+  wget -qO - http://apt.tvheadend.org/repo.gpg.key | sudo apt-key add -
+  echo 'deb http://apt.tvheadend.org/stable wheezy main' > $tvheadend_list
+fi
+
+apt-get update
+
+echo 'Installing packages ...'
+# main system
+apt-get install -y desktop-base k3b kde-workspace ktorrent nfs-kernel-server pavucontrol plymouth plymouth-themes pm-utils quassel-core
+# hardware support
+apt-get install -y firmware-linux-nonfree mesa-vdpau-drivers
+# xbmc
+apt-get install -y tvheadend xbmc xbmc-pvr-tvheadend-hts
+# localization
+apt-get install kde-l10n-sl k3b-i18n
+
+# cleanup
+apt-get clean
+
+echo 'Additional configuration ...'
+# prepare grub for themes
+if grep -q 'GRUB_CMDLINE_LINUX_DEFAULT="quiet"' $grub_default; then
+  echo 'Enabling splash ...'
+  sed -i.bak 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/' $grub_default
+  grub_update=1
+fi
+if grep -q 'GRUB_TIMEOUT=5' $grub_default; then
+  echo 'Disabling countdown timer ...'
+  sed -i.bak 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' $grub_default
+  grub_update=1
+fi
+# update grub if changes were made
+if [[ $grub_update -eq 1 ]]; then
+  echo 'Updating grub ...'
+  update-grub
+fi
+
+# plymouth
+if [ "$(/usr/sbin/plymouth-set-default-theme)" == "lines" ]; then
+  echo "Boot theme already set to lines!"
+elif /usr/sbin/plymouth-set-default-theme --list | grep -q lines; then
+  echo "Setting boot theme to lines ..."
+  /usr/sbin/plymouth-set-default-theme -R lines
+else
+  echo 'Cannot set plymouth theme. Check if desktop-base is installed!'
+fi
+
+# nfs-exports
+if grep -q "$homedir 192.168.1.0/24(rw,nohide,insecure,no_subtree_check,async)" $nfs_exports; then
+  echo 'NFS exports already set!'
+else
+  echo 'Setting NFS exports ...'
+  echo "$homedir 192.168.1.0/24(rw,nohide,insecure,no_subtree_check,async)" >> $nfs_exports
+fi
+
+# autostart XBMC on boot
+if [[ -d "$homedir.kde/" ]]; then
+  if [[ -L "$xbmc_autostart" ]]; then
+      echo 'XMBC already set to autostart!'
+  else
+    echo 'Enabling XBMC autostart ...'
+    ln -s /usr/bin/xbmc $xbmc_autostart
+  fi
+else
+  echo 'Start KDE once and restart this script! XBMC not set to autostart'
+fi
